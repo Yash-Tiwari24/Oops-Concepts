fd4ccecd0d0a10b7830ae7e4286c577f6848d533
Yash-Tiwari24 <yash.t@thinkbridge.in>
2022-02-25 13:54:05 +0530

    Dynamic / Runtime Polymorphism

---
+++ Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Concepts
+{
+    public class Circle : Drawing
+    {
+        public double Radius { get; set; }
+        public Circle()
+        {
+            Radius = 5;
+        }
+        public override double Area()
+        {
+            return (3.14) * Math.Pow(Radius, 2);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,12 +6,25 @@ namespace Oops_Concepts
     {
         static void Main(string[] args)
         {
+            //Static or Compile Time Polymorphism 
             Calculate dataClass = new Calculate();
             int add2 = dataClass.Add(45, 34, 67);
             int add1 = dataClass.Add(23, 34);
 
             Console.WriteLine(add2);
             Console.WriteLine(add1);
+
+
+
+            //Dynamic or Runtime Polymorphism
+            Drawing circle = new Circle();
+            Console.WriteLine("Area :" + circle.Area());
+
+            Drawing square = new Square();
+            Console.WriteLine("Area :" + square.Area());
+
+            Drawing rectangle = new Rectangle();
+            Console.WriteLine("Area :" + rectangle.Area());
         }
     }
 }
