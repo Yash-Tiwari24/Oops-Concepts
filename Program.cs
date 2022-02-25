dd1d1aa941472ab4080774e6c6cee471ba001b59
Yash-Tiwari24 <yash.t@thinkbridge.in>
2022-02-25 18:30:01 +0530

    boxing and unboxing

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace Oops_Concepts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            int num = 2020;
+
+            // boxing
+            object obj = num;
+
+            // value of num to be change
+            num = 100;
+
+            System.Console.WriteLine
+            ("Value - type value of num is : {0}", num);
+            System.Console.WriteLine
+            ("Object - type value of obj is : {0}", obj);
+
+
+            // unboxing
+            int i = (int)obj;
+
+            Console.WriteLine("Value of ob object is : " + obj);
+            Console.WriteLine("Value of i is : " + i);
+
+
+
         }
     }
 }
