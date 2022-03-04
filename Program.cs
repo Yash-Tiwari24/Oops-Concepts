174d3c25718d7e61d4187bd1e118f4218b687432
Yash-Tiwari24 <yash.t@thinkbridge.in>
2022-03-04 13:39:56 +0530

    Added Method

--- Program.cs
+++ Program.cs
@@ -8,41 +8,53 @@ namespace Oops_Concepts
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+
+            Task task = new Task();
+            List<Task> tasks = task.GetTasks();
+
+
+
+
+
 
             //function which will return number of task assigned to a user on particular day
-            int count= ParticularDateCount(Convert.ToDateTime("02-02-2022"));
-            Console.WriteLine("Particular Date Of Task Count \t" +count);
+            int count = program.ParticularDateCount(Convert.ToDateTime("02-02-2022"), tasks);
+            Console.WriteLine("Particular Date Of Task Count \t" + count);
 
 
-            Console.WriteLine("\t");
+            //Console.WriteLine("\t");
 
 
             //function which will return list of task for particular status
-            var status = ParticularStatus("New");
+            var status = program.ParticularStatus("New", tasks);
+            Console.WriteLine();
+            Console.WriteLine("Particular Task Status");
             Console.WriteLine("Task id \t Task Name\t");
             foreach (var item in status)
             {
                 Console.Write(item.Taskid + "\t");
-                Console.Write("\t"+item.TaskName);
+                Console.Write("\t" + item.TaskName);
                 Console.WriteLine();
 
             }
 
             Console.WriteLine("\t");
-
+            Console.WriteLine("Task By Project Name ");
             //function which will search task by its name
-            var name = TaskByName("Create Project");
+            var name = program.TaskByName("Create Project", tasks);
             foreach (var item in name)
             {
                 Console.WriteLine(item.AssignTo);
-                
+
             }
 
 
             Console.WriteLine("\t");
 
-           //function which will return task base on task id
-            var taskid = TaskById(1);
+            //function which will return task base on task id
+            Console.WriteLine("Task By Id ");
+            var taskid = program.TaskById(1, tasks);
             foreach (var item in taskid)
             {
                 Console.WriteLine(item.TaskName);
@@ -51,7 +63,8 @@ namespace Oops_Concepts
             Console.WriteLine("\t");
 
             //function which will return task list for particular date range
-            var daterange = TaskByDateRange(Convert.ToDateTime("02-02-2022"), Convert.ToDateTime("28-02-2022"));
+            Console.WriteLine("Task By Date Range");
+            var daterange = program.TaskByDateRange(Convert.ToDateTime("02-02-2022"), Convert.ToDateTime("28-02-2022"), tasks);
             foreach (var item in daterange)
             {
                 Console.WriteLine(item.TaskName);
@@ -59,56 +72,116 @@ namespace Oops_Concepts
 
             Console.WriteLine("\t");
 
+            Console.WriteLine("Task By Only Name");
+            var onlytaskname = program.TaskByOnlyName(tasks);
+            foreach (var item in onlytaskname)
+            {
+                Console.Write(item.Taskid);
+
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\t");
+
+            Console.WriteLine("Total Number Of Task by Amit \t" + program.TaskByAssignNameCount("Amit", tasks));
+
+            Console.WriteLine("\t");
+
+            Console.WriteLine("Task By Particular Name");
+            var taskbyname = program.TaskByAssignName("swati", tasks);
+            foreach (var item in taskbyname)
+            {
+                Console.WriteLine(item.TaskName);
+            }
+
+            Console.WriteLine("\t");
+
+            Console.WriteLine("Unique Task Name");
+            var taskbyuniquename = program.TaskByUniqueName(tasks);
+            foreach (var item in taskbyuniquename)
+            {
+
+                Console.WriteLine(item);
+
+            }
+
 
         }
 
         //function which will return number of task assigned to a user on particular day 
-        public static int ParticularDateCount(DateTime date)
+        public int ParticularDateCount(DateTime date,List<Task> tasks)
         {
-            var task = Task.GetProducts();
+            
+            
 
-            return task.Where(task => task.Start_Date.Equals(date)).Count();
+            return tasks.Where(task => task.Start_Date.Equals(date)).Count();
         }
 
 
         //function which will return list of task for particular status
-        public static IEnumerable<Task> ParticularStatus(string status)
+        public IEnumerable<Task> ParticularStatus(string status, List<Task> tasks)
         {
-            var task = Task.GetProducts();
-            return task.Where(task => task.Status.Equals(status)).Select(task => task);
+           
+            return tasks.Where(task => task.Status.Equals(status)).Select(task => task);
         }
 
         //create function which will search task by its name
-        public static IEnumerable<Task> TaskByName(string name)
+        public IEnumerable<Task> TaskByName(string name, List<Task> tasks)
         {
-            var task = Task.GetProducts();
-            return task.Where(task => task.TaskName.Equals(name)).Select(task => task);
+           
+            return tasks.Where(task => task.TaskName.Equals(name)).Select(task => task);
         }
 
 
         //function which will return task base on task id
-        public static IEnumerable<Task> TaskById(int id)
+        public IEnumerable<Task> TaskById(int id, List<Task> tasks)
         {
-            var task = Task.GetProducts();
-            return task.Where(task => task.Taskid.Equals(id)).Select(task => task);
+           
+            return tasks.Where(task => task.Taskid.Equals(id)).Select(task => task);
 
         }
 
 
 
         //function which will return task list for particular date range
-        public static IEnumerable<Task> TaskByDateRange(DateTime startdate,DateTime enddate)
+        public IEnumerable<Task> TaskByDateRange(DateTime startdate,DateTime enddate, List<Task> tasks)
         {
-           var task = Task.GetProducts();
-            return from t1 in task
+          
+            return from t1 in tasks
                    where startdate <= t1.Start_Date
                    where enddate >= t1.Start_Date
                    select t1;
         }
 
+        public IEnumerable<Task> TaskByOnlyName(List<Task> tasks)
+        {
+            return tasks.Where(t => !t.TaskName.Equals("")) .Select(t => t);
 
+          
+        }
 
+        public int TaskByAssignNameCount(string assignname, List<Task> tasks)
+        {
 
+           return tasks.Where(task => task.AssignTo.Equals(assignname)).Count();
+            
+        }
+
+     
+        public IEnumerable<Task> TaskByAssignName(string assignname, List<Task> tasks)
+        {
+
+          return tasks.Where(t => t.AssignTo.Equals(assignname));
+
+        }
+
+        public IEnumerable<string> TaskByUniqueName(List<Task> tasks)
+        {
+
+            return tasks.Select(t => t.TaskName).Distinct();
+
+        }
 
     }
 }
