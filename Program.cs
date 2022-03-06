aa095a350bdccb6f1a98b77268f9a18945e56440
Yash-Tiwari24 <yash.t@thinkbridge.in>
2022-03-06 12:42:05 +0530

    Pattern matching Null checks Type tests Relational patterns

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Oops_Concepts
 {
@@ -6,7 +8,54 @@ namespace Oops_Concepts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int? maybe =null;
+
+            if (maybe is int number)
+            {
+                Console.WriteLine($"The nullable int 'maybe' has the value {number}");
+            }
+            else
+            {
+                Console.WriteLine("The nullable int 'maybe' doesn't hold a value");
+            }
+
+            string? message = "This is not the null string";
+            if(message is not null)
+            {
+                Console.WriteLine(message);
+            }
+
+            Student student = new Student();
+            if(student is Student)
+            {
+                Console.WriteLine(student.Name);
+            }
+
+            string WaterState(int tempInFahrenheit) =>
+                tempInFahrenheit switch
+                {
+                    (> 32) and (< 212) => "liquid",
+                    < 32 => "solid",
+                    > 212 => "gas",
+                    32 => "solid/liquid transition",
+                    212 => "liquid / gas transition",
+                };
+            Console.WriteLine(WaterState(32));
+
+            int[] numberList = { 11,12,13,14,15,16,17,18 };
+            Console.WriteLine(StaticMethods.MidPoint(numberList));
+            Console.WriteLine(StaticMethods.MidPoint(new List<int> { 11, 12, 13, 14, 15, 16, 17, 18}));
+            try
+            {
+                Console.WriteLine(StaticMethods.MidPoint(new List<int> { }));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
+
+
     }
 }
---
+++ StaticMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Concepts
+{
+    class StaticMethods
+    {
+        public static T MidPoint<T>(IEnumerable<T> sequence)
+        {
+            if (sequence is IList<T> list)
+            {
+                return list[list.Count / 2];
+            }
+            else if (sequence is null)
+            {
+                throw new ArgumentNullException(nameof(sequence), "Sequence can't be null.");
+            }
+            else
+            {
+                int halfLength = sequence.Count() / 2 - 1;
+                if (halfLength < 0) halfLength = 0;
+                return sequence.Skip(halfLength).First();
+            }
+        }
+    }
+}
